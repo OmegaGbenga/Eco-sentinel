ae8d714a019c050018f18d7f9b808cfe56495d79
OmegaGbenga <Gbengadareowolabi@gmail.com>
2025-12-29 15:29:39 +0100

    feat: add logic to useLocalStorage.ts - section 15

--- frontend/src/hooks/useLocalStorage.ts
+++ frontend/src/hooks/useLocalStorage.ts
@@ -13,3 +13,4 @@ export const useLocalStorage = (key: string, initialValue: any) => {
       const valueToStore = value instanceof Function ? value(storedValue) : value;
       setStoredValue(valueToStore);
       window.localStorage.setItem(key, JSON.stringify(valueToStore));
+    } catch (error) {
