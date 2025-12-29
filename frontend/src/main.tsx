dd86f0b32012385d611ab7f19df57fd1360fef47
OmegaGbenga <Gbengadareowolabi@gmail.com>
2025-12-29 14:51:03 +0100

    feat: add logic to main.tsx - section 2

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -12,3 +12,5 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
