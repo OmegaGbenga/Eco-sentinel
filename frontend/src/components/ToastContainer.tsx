40d343a268c55e6cb7d8598598db8944852970ca
OmegaGbenga <Gbengadareowolabi@gmail.com>
2025-12-29 15:16:47 +0100

    feat: add logic to ToastContainer.tsx - section 2

--- frontend/src/components/ToastContainer.tsx
+++ frontend/src/components/ToastContainer.tsx
@@ -2,3 +2,5 @@ import React from 'react';
 import { useNotifications } from '../context/NotificationContext';
 
 export const ToastContainer: React.FC = () => {
+  const { notifications, removeNotification } = useNotifications();
+
