425f7eae3e81ebadf4299faabe8a56bac4ad53d3
OmegaGbenga <Gbengadareowolabi@gmail.com>
2025-12-29 15:29:19 +0100

    feat: add logic to Accordion.tsx - section 8

--- frontend/src/components/ui/Accordion.tsx
+++ frontend/src/components/ui/Accordion.tsx
@@ -1,2 +1,9 @@
 import React, { useState } from 'react';
 export const Accordion: React.FC<any> = ({ title, children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div className='accordion'>
+      <div onClick={() => setIsOpen(!isOpen)}>{title}</div>
+      {isOpen && <div>{children}</div>}
+    </div>
+  );
