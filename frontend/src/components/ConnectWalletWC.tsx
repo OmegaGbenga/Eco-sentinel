c485268845b4ebda5ff2e17b1236c4178ce403e5
OmegaGbenga <Gbengadareowolabi@gmail.com>
2025-12-29 14:52:47 +0100

    feat: add logic to ConnectWalletWC.tsx - section 2

--- frontend/src/components/ConnectWalletWC.tsx
+++ frontend/src/components/ConnectWalletWC.tsx
@@ -2,3 +2,5 @@ import React, { useState } from 'react';
 import { initWalletConnect, pair } from '../services/walletConnectService';
 
 export const ConnectWalletWC: React.FC = () => {
+  const [uri, setUri] = useState('');
+
