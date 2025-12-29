220d8c62290c1be1e4226d79fa68f1849f773ca4
OmegaGbenga <Gbengadareowolabi@gmail.com>
2025-12-29 14:52:38 +0100

    feat: add logic to ChainhooksView.tsx - section 2

--- frontend/src/components/ChainhooksView.tsx
+++ frontend/src/components/ChainhooksView.tsx
@@ -2,3 +2,5 @@ import React from 'react';
 import { ChainhookMonitor } from '../services/chainhooksService';
 
 export const ChainhooksView: React.FC = () => {
+  const monitor = new ChainhookMonitor();
+
