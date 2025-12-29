3d01fae672882e68fe40110f39cd056e6c433d56
OmegaGbenga <Gbengadareowolabi@gmail.com>
2025-12-29 14:51:11 +0100

    feat: add logic to App.tsx - section 2

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -37,3 +37,5 @@ import React, { useState, useEffect } from 'react';
 import { AppConfig, UserSession, showConnect } from '@stacks/connect';
 import { StacksTestnet } from '@stacks/network';
 
+const appConfig = new AppConfig(['store_write', 'publish_data']);
+const userSession = new UserSession({ appConfig });
