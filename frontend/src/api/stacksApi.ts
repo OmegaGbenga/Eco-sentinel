5da4c1680c0e945dd1eaba937e871883ce1e51cb
OmegaGbenga <Gbengadareowolabi@gmail.com>
2025-12-29 15:25:16 +0100

    feat: add logic to stacksApi.ts - section 7

--- frontend/src/api/stacksApi.ts
+++ frontend/src/api/stacksApi.ts
@@ -5,3 +5,4 @@ const network = new StacksMainnet();
 export const getAccountBalances = async (address: string) => {
   const response = await fetch(`${network.coreApiUrl}/extended/v1/address/${address}/balances`);
   return response.json();
+};
