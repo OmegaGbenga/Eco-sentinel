2f809afd262b35c7154aaf5694c6afd7c0a68d60
OmegaGbenga <Gbengadareowolabi@gmail.com>
2025-12-29 15:23:00 +0100

    feat: add logic to useDebounce.ts - section 6

--- frontend/src/hooks/useDebounce.ts
+++ frontend/src/hooks/useDebounce.ts
@@ -4,3 +4,4 @@ export function useDebounce<T>(value: T, delay: number): T {
   const [debouncedValue, setDebouncedValue] = useState<T>(value);
 
   useEffect(() => {
+    const handler = setTimeout(() => {
