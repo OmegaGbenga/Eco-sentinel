81d20d3da246d30fc724cf7f63ac70a8bde8dc2b
OmegaGbenga <Gbengadareowolabi@gmail.com>
2025-12-29 15:26:20 +0100

    feat: add logic to Sidebar.test.tsx - section 4

--- tests/components/Sidebar.test.tsx
+++ tests/components/Sidebar.test.tsx
@@ -2,3 +2,4 @@ import { render, screen } from '@testing-library/react';
 import { Sidebar } from '../../frontend/src/components/Sidebar';
 
 test('renders dashboard link', () => {
+  render(<Sidebar />);
