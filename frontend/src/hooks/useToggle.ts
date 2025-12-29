617b8340f22d3ca57644ec79744fe79659bd94aa
OmegaGbenga <Gbengadareowolabi@gmail.com>
2025-12-29 15:29:50 +0100

    feat: add logic to useToggle.ts - section 3

--- frontend/src/hooks/useToggle.ts
+++ frontend/src/hooks/useToggle.ts
@@ -1,3 +1,4 @@
 import { useState, useCallback } from 'react';
 export const useToggle = (initialState: boolean = false) => {
   const [state, setState] = useState(initialState);
+  const toggle = useCallback(() => setState(state => !state), []);
