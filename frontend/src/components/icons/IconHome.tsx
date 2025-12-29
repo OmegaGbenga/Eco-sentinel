b021696707c8c1371354c6c7a69e46be026e18bf
OmegaGbenga <Gbengadareowolabi@gmail.com>
2025-12-29 15:22:45 +0100

    feat: add logic to IconHome.tsx - section 3

--- frontend/src/components/icons/IconHome.tsx
+++ frontend/src/components/icons/IconHome.tsx
@@ -1,3 +1,4 @@
 import React from 'react';
 export const IconHome = () => (
   <svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" strokeWidth="2" fill="none">
+    <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
