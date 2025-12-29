4bd19dc8977ad059e0081aa35963fc7bc4751b2f
OmegaGbenga <Gbengadareowolabi@gmail.com>
2025-12-29 15:29:34 +0100

    feat: add logic to useLocalStorage.ts - section 9

--- frontend/src/hooks/useLocalStorage.ts
+++ frontend/src/hooks/useLocalStorage.ts
@@ -2,3 +2,9 @@ import { useState } from 'react';
 export const useLocalStorage = (key: string, initialValue: any) => {
   const [storedValue, setStoredValue] = useState(() => {
     try {
+      const item = window.localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      return initialValue;
+    }
+  });
