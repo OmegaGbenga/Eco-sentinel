ec3cf68608a68f101d029c1257f9c514460bcf70
OmegaGbenga <Gbengadareowolabi@gmail.com>
2025-12-29 14:53:26 +0100

    feat: add logic to App.tsx - section 1

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,2 +1,4 @@
 import React, { useState, useEffect } from 'react';
 import { AppConfig, UserSession, showConnect } from '@stacks/connect';
+import { ConnectWalletWC } from './components/ConnectWalletWC';
+import { ChainhooksView } from './components/ChainhooksView';
