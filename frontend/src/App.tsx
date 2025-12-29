4c4ba1268dc77c7663e26e1354d66c08fe9c8269
OmegaGbenga <Gbengadareowolabi@gmail.com>
2025-12-29 14:53:28 +0100

    feat: add logic to App.tsx - section 3

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,3 +4,5 @@ import { ConnectWalletWC } from './components/ConnectWalletWC';
 import { ChainhooksView } from './components/ChainhooksView';
 
 const appConfig = new AppConfig(['store_write', 'publish_data']);
+const userSession = new UserSession({ appConfig });
+
