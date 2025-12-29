e0f730e3dc2f0ef79d60b2cc3dfcfdeefb904edc
OmegaGbenga <Gbengadareowolabi@gmail.com>
2025-12-29 15:18:17 +0100

    feat: add logic to Admin.tsx - section 1

--- frontend/src/pages/Admin.tsx
+++ frontend/src/pages/Admin.tsx
@@ -1,2 +1,4 @@
 import React from 'react';
 import { DataChart } from '../components/DataChart';
+import { generateMockData } from '../data/mockData';
+import { ToastContainer } from '../components/ToastContainer';
