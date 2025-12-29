ab4265cbe6dcfda6824063a0c5c4ed676da0f06e
OmegaGbenga <Gbengadareowolabi@gmail.com>
2025-12-29 15:27:56 +0100

    feat: add logic to Tooltip.tsx - section 1

--- frontend/src/components/ui/Tooltip.tsx
+++ frontend/src/components/ui/Tooltip.tsx
@@ -1,1 +1,2 @@
 import React from 'react';
+export const Tooltip: React.FC<any> = ({ text, children }) => <div className='tooltip-container'>{children}<span className='tooltip-text'>{text}</span></div>;
