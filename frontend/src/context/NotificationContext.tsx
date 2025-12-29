2e0fc17ca4e8bd281b6c06467c082ee38183afc1
OmegaGbenga <Gbengadareowolabi@gmail.com>
2025-12-29 15:16:43 +0100

    feat: add logic to NotificationContext.tsx - section 21

--- frontend/src/context/NotificationContext.tsx
+++ frontend/src/context/NotificationContext.tsx
@@ -40,3 +40,4 @@ export const useNotifications = () => {
   const context = useContext(NotificationContext);
   if (!context) throw new Error('useNotifications must be used within a NotificationProvider');
   return context;
+};
